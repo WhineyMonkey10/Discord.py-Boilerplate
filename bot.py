c585effb616ac9e2ebb7c3c69178221da9d4713a
GBJ <gbialystokjoly@gmail.com>
2023-05-06 17:09:57 +0200

    init. commit

--- bot.py
+++ bot.py
@@ -3,3 +3,59 @@
 
 # Imports
 
+## Discord Imports
+
+import discord
+from discord.ext import commands
+
+## System Imports
+
+import os
+
+## Connfiguration Imports
+
+from dotenv import load_dotenv
+
+## Other Imports
+
+import colorama
+import random
+
+
+### Setup Configurations
+
+botToken = str(os.getenv("TOKEN"))
+botPrefix = os.getenv("PREFIX")
+botStatus = os.getenv("STATUS")
+
+## Main Code (For now, no comments or clean code will be added beyond this point)
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True
+intents.guilds = True
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.guild_typing = True
+intents.dm_messages = True
+intents.dm_reactions = True
+intents.dm_typing = True
+intents.bans = True
+intents.emojis = True
+intents.integrations = True
+intents.invites = True
+intents.voice_states = True
+intents.webhooks = True
+
+bot = commands.Bot(command_prefix=botPrefix, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(colorama.Fore.GREEN + f"Logged in as {bot.user.name}#{bot.user.discriminator} ({bot.user.id})")
+    await bot.change_presence(activity=discord.Game(name=botStatus))
+    print(colorama.Fore.GREEN + f"Status set to {botStatus}")
+    
+
+bot.run(botToken)
+    
